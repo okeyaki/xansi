29b079b21925bb32246c38ac71f31ff9dc64cec5
okeyaki <shinya.otsuki.4d@gmail.com>
2022-01-30 16:37:53 +0900

    Support options

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
+extern crate clap;
 extern crate xansi;
 
 use std::io::stdin;
 use std::io::BufRead;
 use std::io::BufReader;
+use clap::App;
 
 fn main() {
+    let _ = App::new("xansi")
+        .about("A CLI tool to remove ANSI escape sequences")
+        .version("0.2.0")
+        .get_matches();
+
     let mut stream = BufReader::new(stdin());
     loop {
         let mut line = String::new();
